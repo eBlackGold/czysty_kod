c2e27c5f0ccb8367108417b9e82dd5134d5650ec
Martyna Jurczak <45241704+Nausiz@users.noreply.github.com>
2023-01-14 15:53:58 +0100

    Poprawki w layoucie dodawania produktu do koszyka + przygotowanie aktywności do zakodowania funkcjonalności

--- eBlackGold/app/src/main/java/com/example/eblackgold/AddToCart.java
+++ eBlackGold/app/src/main/java/com/example/eblackgold/AddToCart.java
@@ -2,7 +2,13 @@ package com.example.eblackgold;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class AddToCart extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class AddToCart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_cart);
+
+        ImageView imgOfProduct = findViewById(R.id.imgAddToCart);
+
+        TextView productName = findViewById(R.id.productNameAddToCart);
+        TextView sellerName = findViewById(R.id.sellerNameAddToCart);
+        TextView unitsInStock = findViewById(R.id.unitsAddToCart);
+        TextView price = findViewById(R.id.priceAddToCart);
+        TextView type = findViewById(R.id.typeAddToCart);
+
+        RadioGroup deliveryOptions = findViewById(R.id.radioGroupDeliveryAddToCart);
+        RadioGroup paymentOptions = findViewById(R.id.radioGroupPaymentAddToCart);
+
+        Button addToCartButton = findViewById(R.id.buttonAddToCart);
+
+        //Kod podmieniający wszystkie nazwy itp. na te pobrane z bazy
+
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Kod odpowiadający za dodanie produktu do koszyka
+            }
+        });
     }
 }
