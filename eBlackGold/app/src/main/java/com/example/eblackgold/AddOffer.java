c9a7587ed4e953a45ef661241875068e844422b6
Martyna Jurczak <45241704+Nausiz@users.noreply.github.com>
2023-01-14 16:56:05 +0100

    Poprawki w layoucie dodawania oferty + przygotowanie aktywności do zakodowania funkcjonalności

--- eBlackGold/app/src/main/java/com/example/eblackgold/AddOffer.java
+++ eBlackGold/app/src/main/java/com/example/eblackgold/AddOffer.java
@@ -3,8 +3,15 @@ package com.example.eblackgold;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class AddOffer extends AppCompatActivity {
 
@@ -13,13 +20,42 @@ public class AddOffer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_offer);
 
-        Spinner dropdown = findViewById(R.id.dropdownTypeOffer);
-        //create a list of items for the spinner.
-        String[] items = new String[]{"Typ1", "Typ2", "Typ3"};
-        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        //There are multiple variations of this, but this is the basic variant.
+        ImageView imgOfProduct = findViewById(R.id.imgAddOffer);
+
+        EditText productName = findViewById(R.id.productNameAddOffer);
+        EditText units = findViewById(R.id.unitsAddOffer);
+        EditText price = findViewById(R.id.priceAddOffer);
+
+        Spinner dropdown = findViewById(R.id.dropdownTypeAddOffer);
+        String[] items = new String[]{Integer.toString(R.string.typ1), Integer.toString(R.string.typ2), Integer.toString(R.string.typ3)};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
+
+        //Dostawy
+        CheckBox personalPickup = findViewById(R.id.checkboxPersonalPickup);
+        CheckBox courier = findViewById(R.id.checkboxCourier);
+
+        //Zapłaty
+        CheckBox blik = findViewById(R.id.checkboxBlik);
+        CheckBox transfer = findViewById(R.id.checkboxTransfer);
+        CheckBox cash = findViewById(R.id.checkboxCash);
+
+        Button addOfferButton = findViewById(R.id.buttonAddOffer);
+
+        imgOfProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Kod pobierający zdjęcie od użytkownika
+            }
+        });
+
+        addOfferButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Kod odpowiadający za dodanie oferty do bazy
+            }
+        });
+
+        //Kod podmieniający wszystkie nazwy itp. na te pobrane z bazy
     }
 }
