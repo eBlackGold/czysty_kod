19ed9248bd729ed7d7fa548e025920dc195f041c
Martyna Jurczak <45241704+Nausiz@users.noreply.github.com>
2023-01-16 22:34:25 +0100

    Poprawki w layoucie koszyka + przygotowanie aktywności do zakodowania funkcjonalności

--- eBlackGold/app/src/main/java/com/example/eblackgold/Cart.java
+++ eBlackGold/app/src/main/java/com/example/eblackgold/Cart.java
@@ -3,6 +3,8 @@ package com.example.eblackgold;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cart extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Cart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        Button buttonPay = findViewById(R.id.buttonPayCart);
+
+        buttonPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Kod usuwający rzeczy z koszyka i dodanie ich do zamówień sprzedającego
+            }
+        });
     }
 }
